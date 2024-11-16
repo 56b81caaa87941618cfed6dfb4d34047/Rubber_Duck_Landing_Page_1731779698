65d23c9d7344a09b68366004e8968fa063e57132
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-11-16 12:08:49 -0600

    Delete component: Header1731780259

--- src/ProjectComponents/index.ts
+++ src/ProjectComponents/index.ts
@@ -1,4 +1,3 @@
 export * as Hero1731779701 from './Hero1731779701'
 export * as Header1731779701 from './Header1731779701'
-export * as Header1731780259 from './Header1731780259'
 export * as Footer1731779701 from './Footer1731779701'
